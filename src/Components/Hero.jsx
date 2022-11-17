7c6fcd75e154b58a1f3f0b3ab27f127afd2a4242
scarletMaster <nadinebenarbia@gmail.com>
2022-11-17 15:25:22 +0100

    landingpage navbar+hero section

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <div className='w-full h-screen flex '>
+            <div className='flex flex-col flex-wrap '>
+                <h1 className=" "> Pourquoi vous équiper d’une solution Qœur ?</h1>
+                <h3 className=''>Malheureusement qu’on en soit conscient ou non, un accident peut arriver à chaque instant.</h3>
+            </div>
+            <div>
+                <img src='' alt='' />
+            </div>
+        </div>
+    )
+}
+
+export default Hero
